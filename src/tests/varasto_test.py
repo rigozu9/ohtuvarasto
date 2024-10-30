5efe5684d72592c0b6c0012fe36d17ddd6f8892e
Riku <134082872+rigozu9@users.noreply.github.com>
2024-10-30 12:23:44 +0200

    muutin tiedostoa

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -74,5 +74,6 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
 
     def test_varasto_str_representation(self):
+        print("Hello from tests!")
         self.varasto.lisaa_varastoon(5)
         self.assertEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
